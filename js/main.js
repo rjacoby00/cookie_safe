b3cbcdfa7e6404cde6af9420ff9928753e5f69b5
Ryan Jacoby <rjacoby00@icloud.com>
2019-05-24 12:20:58 -0700

    Add other end of new debugging code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var former = console.log;
+console.log = function(msg){
+    former(msg);  //maintains existing logging via the console.
+    $("#mylog").append("<div>" + msg + "</div>");
+}
+
+window.onerror = function(message, url, linenumber) {
+    console.log("JavaScript error: " + message + " on line " + 
+            linenumber + " for " + url);
+}
+
 function logCookies(cookies) {
   document.getElementById("text_imput").value = cookies;
 }
